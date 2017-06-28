a6efa57a5b70c35f87b20d2d865c045200b535f5
Thornton Prime <thornton.prime@gmail.com>
2017-06-28 08:00:01 -0700

    FIxes to Titles scrubber

--- bin/titles.sh
+++ bin/titles.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+(
+
 bin/rengu verses/* | \
-  jq -r .Source.Source.Title 2>/dev/null | \
+  jq -r .Source.Title 2>/dev/null
+
+bin/rengu verses/* | \
+  jq -r .Source.Source.Title 2>/dev/null
+
+) | \
   grep -v null | \
-  sed -e 's/^ *//' | \
+  sed -e 's/^ *//' | sed -e 's/"//g' | sed -e 's/,$//' | \
   sort | uniq
+
