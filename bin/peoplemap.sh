6703d7c151064fbf0bef3763b3b6e3f9eff44f67
Thornton Prime <thornton.prime@gmail.com>
2017-11-17 15:08:43 -0800

    People map and wikipedia search

---
+++ bin/peoplemap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bin/rengu people verses/* | \
+  grep -v null | \
+  sed -e 's/.*://' | \
+  tr -d '",:' | \
+  sed -e 's/[[:space:]][[:space:]]*/ /g' | \
+  sort | uniq | \
+  bin/wikipedia > results/peoplemap.txt
+
