22425d9a324943f3605a2ec45d1abf0dc500e5cc
Thornton Prime <thornton.prime@gmail.com>
2018-02-05 10:50:33 -0800

    Daily for Feb 5

--- bin/last_people.sh
+++ bin/last_people.sh
@@ -41,7 +41,9 @@ grep -l 503bbcad-73dd-49e6-96a0-bde44020aeb4 verses/* | \
 
 # Manhae
 grep -l e13d8994-eff6-4830-981a-b30658d44b81 verses/* |
-  xargs bin/rengu json | jq -r '._id + " " + (.Source.Locus.Page | tostring)' | sort -k 2g 
+  xargs bin/rengu json | \
+  jq -r '._id + " " + (.Source.Locus.Number | tostring) + " " + (.Source.Locus.Page | tostring)' | \
+  sort -k 2g -k 3g
 
 # Longing
 grep -l 82a0aaf9-3f47-4e8c-9075-b14ae205f2a5 verses/* |
