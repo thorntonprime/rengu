14bac6399a9466edfc159ce2aa3211e0f1cd1104
Thornton Prime <thornton.prime@gmail.com>
2018-02-09 12:00:23 -0800

    Incremental updates

---
+++ rengu/config.py
@@ -0,0 +1,8 @@
+broker_url = 'redis://prajna'
+result_backend = 'redis://prajna'
+
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Pacific/Los_Angeles'
+enable_utc = True
--- tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-app = Celery('rengu', broker='redis://prajna', backend='redis://prajna')
-
-@app.task
-def add(x, y):
-    return x + y
