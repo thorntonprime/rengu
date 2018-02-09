ce03909930db02444433fbdca1addf218d5f4270
Thornton Prime <thornton.prime@gmail.com>
2018-02-09 15:13:29 -0800

    Fix daemon options

--- bin/celery.sh
+++ bin/celery.sh
@@ -1,4 +1,46 @@
-#!/bin/sh
+#!/bin/bash
+# 
 
-celery-3 -A rengu worker --loglevel=info -E
+RETVAL=0
+
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+
+usage ()
+{
+	echo $"Usage: $0 {start|stop|status|restart|condrestart}" 1>&2
+	RETVAL=2
+}
+
+stop ()
+{
+    celery-3 multi stop
+    rm tmp/celery-*.pid
+}
+
+status ()
+{
+    celery-3 multi show
+}
+
+start () 
+{
+    celery-3 multi start 10 \
+        -A rengu \
+        -l info -c4 \
+        -f %n-%i.log \
+        --pidfile=tmp/celery-%n.pid
+}
+
+case "$1" in
+    stop) stop ;;
+    status) status ;;
+    start) start ;;
+    restart|reload|force-reload) restart ;;
+    # condrestart) condrestart ;;
+    *) usage ;;
+esac
+
+exit $RETVAL
 
