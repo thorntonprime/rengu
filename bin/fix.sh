9ac715ebea7eabe620add21a89a01c351f81c1f6
Thornton Prime <thornton.prime@gmail.com>
2018-03-01 10:55:00 -0800

    Fix fixit script for wrapped words

--- bin/fix.sh
+++ bin/fix.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cat | \
-  fmt -w 80 | \
-  awk '{ print $0 " " }' | \
+  sed -e 's/[——-]$/_/g' | \
+  tr '\n' ' ' | \
   sed -e 's/  */ /g' | \
   sed -e 's/  *\. *\. *\. */ ... /g' | \
   sed -e 's/…/ .../g' | \
@@ -10,10 +10,10 @@ cat | \
   sed -e 's/\([^\.]\)\.  *\([a-z]\)/\1, \2/g' | \
   sed -e 's/\([A-Za-z\.,;:-]\)  *\([A-Za-z-]\)/\1 \2/g' | \
   sed -e 's/\([a-z]\)—  */\1 -- /g' | \
-  sed -e 's/ *—$/ --/g' | \
   sed -e 's/  *o  *f  */ of /g' | \
   sed -e 's/:/：/g' | \
   sed -e 's/[——-]/-/g' | \
+  sed -s 's@\_ *@@' | \
   sed -e 's/ *$//g' | \
-  fmt -w 80 | \
-  sed -e 's/  */ /g'
+  sed -e 's/  */ /g' | \
+  fmt -w 78
