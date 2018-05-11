d498565cdfc12baed1979bb4ab3a46d940d7656d
Thornton Prime <thornton.prime@yoyoweb.com>
2018-05-11 13:24:19 -0700

    MacOS work-arounds

--- lib/prajna/rengu/config.py
+++ lib/prajna/rengu/config.py
@@ -4,8 +4,13 @@ import getpass
 if getpass.getuser() in [ 'rengu', 'uwsgi' ]:
     RENGUPATH='/srv/rengu'
 
+elif os.uname()[0] == 'Darwin':
+    RENGUPATH = os.environ.get("HOME",".") + '/Projects/rengu'
+
 else:
     RENGUPATH = os.environ.get("HOME",".") + '/projects/rengu'
+
+
 
 # Celery Configuration
 broker_url = 'redis://prajna'
