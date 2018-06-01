582132b3ea71bc4180c60d1eff2df416980b6852
Thornton Prime <thornton.prime@gmail.com>
2018-06-01 13:36:06 -0700

    Daily

--- lib/rengu/local.py
+++ lib/rengu/local.py
@@ -7,14 +7,16 @@ class Repository:
     def __init__(self, path=os.getenv("RENGUPATH", os.getcwd())):
         # Repository(RENGUPATH)
         #   Return a local repository object
+        import sh
+
         self.RENGU_PATH = path
+        self.git = sh.git.bake("-C", self.RENGU_PATH, "--no-pager")
 
     def updated_files(self):
         # updated_files()
         #   Return a generator of the updated (uncommitted) files in the local
         #   repository
-        import sh
-        for f in sh.git("-C", self.RENGU_PATH, "--no-pager", "diff", "--name-only"):
+        for f in self.git("diff", "--name-only"):
             yield f.strip()
 
     def updated_data(self):
@@ -24,12 +26,16 @@ class Repository:
                 if d in ["authors", "sources", "verses"]:
                     yield "/".join(d,i)
 
-    def push_all(self, msg):
-        import sh
-        git_all=sh.git("-C", self.RENGU_PATH, "commit", "-m", msg, *self.updated_files())
+    def commit_all(self, msg):
+        self.git("commit", "-m", msg, *self.updated_files())
 
     def commit_data(self, msg):
-        import sh
-        git_all=sh.git("-C", self.RENGU_PATH, "commit", "-m", msg, *self.updated_files())
+        self.git("commit", "-m", msg, *self.updated_data())
 
+    def push_commits(self):
+        self.git("push")
+
+    def push_data(self):
+        self.commit_data("daily")
+        self.push_commits()
 
