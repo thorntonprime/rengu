8d6465a40c8716c27c80c8b30fc1bcd618b33038
Thornton Prime <thornton.prime@gmail.com>
2018-06-14 16:47:54 -0700

    Working parallel lint

--- lib/rengu/cli/__init__.py
+++ lib/rengu/cli/__init__.py
@@ -10,6 +10,7 @@ def data_files(repo, data):
     ALL_AUTHORS All author data files
     ALL_SOURCES All source data files
     ALL_VERSES  All verse data files
+    ALL_PRINTS  All print data files
     '''
 
     if len(data) < 1:
@@ -27,36 +28,27 @@ def data_files(repo, data):
             return list(repo.all_data(['authors']))
         elif data[0] == 'ALL_SOURCES':
             return list(repo.all_data(['sources']))
+        elif data[0] == 'ALL_PRINTS':
+            return list(repo.all_data(['prints']))
     
     else:
         return data
 
-def show_verbose(msg):
-
-    def echo_verbose(i):
-        for path in i:
-            typs, uid = path.split('/', 1)
-            click.echo('%s [%s] %s' % (uid, typs[:-1], msg))
-            yield path
-
-    return echo_verbose
-
-def show(verbose, progress, msg):
+def show(verbose, progress):
 
     if verbose and progress:
         import sys
         click.echo("Error: can't speficy both verbose and progress")
         sys.exit(1)
 
-    elif verbose:
-        return show_verbose(msg)
-
     elif progress:
         from tqdm import tqdm
         return tqdm
 
     else:
-        return lambda x : x
+        def _null(x, total=None):
+            return x
+        return _null
 
 @click.group()
 def cli():
@@ -67,26 +59,35 @@ def cli():
 @click.option('--verbose', '-v', envvar='RENGUVERBOSE', default=0, count=True)
 @click.option('--progress / --no-progress', is_flag=True, default=False)
 @click.argument('data', nargs=-1, type=click.Path(), required=True)
-def check2(data, path, verbose, progress):
+def lint(data, path, verbose, progress):
     '''Check integrity of data file(s)'''
     from yamllint.config import YamlLintConfig
     from yamllint.linter import run
+
+    from tqdm import tqdm
     from dask.distributed import Client, as_completed
 
     repo=Repository(path)
     client = Client()
    
     fs = data_files(repo, data) 
+    total = len(fs)
+
+    yconfig = YamlLintConfig('extends: default')
 
     def _checker(d):
-        f = open(d)
-        errors = run(f, YamlLintConfig('extends: default'))
-        #x = d.split('-')
-        return (d, [ str(e) for e in errors ])
+        typ, uid = d.split('/', 1)
+        if verbose > 1:
+            print("{0} [{1}] check > opening".format(uid, typ))
+        with open(d) as f:
+            lint_errors = run(f, yconfig)
+            if verbose > 1:
+                print("{0} [{1}] check > closing".format(uid, typ))
+        return [ d + " " + e for e in lint_errors ]
 
     futures = client.map(_checker, fs)
 
-    for f in as_completed(futures):
+    for f in show(verbose, progress)(as_completed(futures), total=total):
         c = f.result()
         for e in c:
             print(e)
@@ -96,7 +97,7 @@ def check2(data, path, verbose, progress):
 @click.option('--verbose', '-v', envvar='RENGUVERBOSE', default=0, count=True)
 @click.option('--progress / --no-progress', is_flag=True, default=False)
 @click.argument('data', nargs=-1, type=click.Path(), required=True)
-def check1(data, path, verbose, progress):
+def check(data, path, verbose, progress):
     '''Check integrity of data file(s)'''
     from rengu.check import check
 
